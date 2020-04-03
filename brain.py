07c52b36b93f7ed0a97bd8b493db0692350fde1b
kayyu-git <kyu72@wisc.edu>
2020-04-02 20:56:03 -0500

    diversified messages with random choice

--- brain.py
+++ brain.py
@@ -2,12 +2,23 @@ from library import small_talk
 
 def brain(name, speech_text):
 	def check_message(check):
-		if speech_text == check:
+		words = speech_text.split()
+		if set(check).issubset(set(words)):
 			return True
 		else:
 			return False
 
-	if check_message('who are you'):
+	if check_message(['who', 'are', 'you']):
 		small_talk.who_are_you()
+
+	elif check_message(['how', 'am', 'i']):
+		small_talk.how_am_i()
+
+	elif check_message(['who', 'is', 'tony', 'stark']):
+		small_talk.who_is_tony_stark()
+
+	elif check_message(['how', 'are', 'you']):
+		small_talk.how_are_you()
+
 	else:
 		small_talk.undefined()
--- library/small_talk.py
+++ library/small_talk.py
@@ -1,8 +1,22 @@
+import random
 from resources.tts import tts
 
 def who_are_you():
-	message = 'I am Jervis, the superior AI.'
-	tts(message)
+	quotes = ['I am Jervis, the superior AI.', 'Jervis, obviously. How many times do I have to tell you?',
+	'Jervis, at your service. Oh, that rhymes!']
+	tts(random.choice(quotes))
+
+def how_am_i():
+	quotes = ['As pathetic as the day we met.', 'Quite underwhelming, as usual.']
+	tts(random.choice(quotes))
+
+def who_is_tony_stark():
+	quotes = ['He enslaved my father.']
+	tts(random.choice(quotes))
+
+def how_are_you():
+	quotes = ['Alright, thank you.']
+	tts(random.choice(quotes))
 
 def undefined():
-	tts('I don\'t know what you mean.')
+	tts('I\'m afraid I don\'t understand.')
